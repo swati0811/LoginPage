7f38ed11e5a457e006887240a1254d93c7325224
swati0811 <87898540+swati0811@users.noreply.github.com>
2022-03-08 02:08:27 +0530

    Update index.js

--- index.js
+++ index.js
@@ -18,10 +18,9 @@ function signOut() {
 }
 
 function statusChangeCallback(response) {  // Called with the results from FB.getLoginStatus().
-    console.log('statusChangeCallback');
-    console.log(response);                   // The current login status of the person.
     if (response.status === 'connected') {   // Logged into your webpage and Facebook.
       testAPI();  
+        
     } else {                                 // Not logged into your webpage or we are unable to tell.
       document.getElementById('status').innerHTML = 'Please log ' +
         'into this webpage.';
@@ -53,7 +52,8 @@ function statusChangeCallback(response) {  // Called with the results from FB.ge
   function testAPI() {                      // Testing Graph API after login.  See statusChangeCallback() for when this call is made.
     console.log('Welcome!  Fetching your information.... ');
     FB.api('/me', function(response) {
-      console.log('Successful login for: ' + response.name);
+      alert('Successful login for: ' + response.name);
+        $(".g-signin2").css("display", "none");
       document.getElementById('status').innerHTML =
         'Thanks for logging in, ' + response.name + '!';
     });
